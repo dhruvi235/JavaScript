43f82cc5a9c3932f5f7655281c79541809bb239f
Dhruvi_235 <dkg56609@gmail.com>
2025-03-10 23:09:30 +0530

    Add files via upload

---
+++ Java Script/Lab 24/message.js
@@ -0,0 +1,10 @@
+const message = () => {
+    const name = "Dhruvi";
+    const age = 18;
+    return name + " is " + age + " years old.";
+};
+
+export default message; 
+
+export const name = "Dhruvi";  
+export const age = 18;        
